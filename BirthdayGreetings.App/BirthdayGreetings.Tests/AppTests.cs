ea6a4f447ce1273d432ff793fb5d030e3bfd069a
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-16 12:41:57 +0200

    remove too many assertion smell

--- BirthdayGreetings.App/BirthdayGreetings.Tests/AppTests.cs
+++ BirthdayGreetings.App/BirthdayGreetings.Tests/AppTests.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using System.Linq;
 using System.Net.Mail;
 using System.Threading.Tasks;
+using BirthdayGreetings.Tests.Support;
+using DeepEqual.Syntax;
 using netDumbster.smtp;
 using Xunit;
 
@@ -12,14 +14,9 @@ namespace BirthdayGreetings.Tests
         //        - hardcoded smtp endpoint in prod code
         //        - hardcoded from address in prod code
         //        - remember to add WhenOneBirthday to test name
-        //        - troppe assert
-        //
-        // confrontare intero oggetto StmpMessage[] + 2
-        // uso Linq Single e tolgo 3 assert + 1
-        // extract utility method AssertMessage(from, to, s, b, smtpMessage) 
 
         [Fact]
-        public async  Task SendOneGreeting()
+        public async Task SendOneGreeting()
         {
             var app = new App();
 
@@ -27,16 +24,11 @@ namespace BirthdayGreetings.Tests
             {
                 await app.Run();
 
-                // ne ho ricevuta 1
-                var smtpMessage = Assert.Single(smtpServer.ReceivedEmail);
-
-                // quella ricevuta ha la forma che mi aspetto
-                Assert.Equal("foo@bar.com", smtpMessage.FromAddress.Address);
-                var toAddress = Assert.Single(smtpMessage.ToAddresses);
-                Assert.Equal("mary.ann@foobar.com", toAddress.Address);
-                Assert.Equal("Happy birthday!", smtpMessage.Headers["Subject"]);
-                var messagePart = Assert.Single(smtpMessage.MessageParts);
-                Assert.Equal("Happy birthday, dear Mary!", messagePart.BodyData);
+               ReceivedMail.FromAll(smtpServer)
+                    .ShouldDeepEqual(new[] { new ReceivedMail("foo@bar.com",
+                        "mary.ann@foobar.com",
+                        "Happy birthday!",
+                        "Happy birthday, dear Mary!") });
             }
         }
     }
---
+++ BirthdayGreetings.App/BirthdayGreetings.Tests/Support/ReceivedMail.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using netDumbster.smtp;
+
+namespace BirthdayGreetings.Tests.Support
+{
+    public class ReceivedMail
+    {
+        readonly String fromAddress;
+        readonly String toAddress;
+        readonly String subject;
+        readonly String body;
+
+        public ReceivedMail(String fromAddress, String toAddress, String subject, String body)
+        {
+            this.fromAddress = fromAddress;
+            this.toAddress = toAddress;
+            this.subject = subject;
+            this.body = body;
+        }
+
+        bool Equals(ReceivedMail other)
+        {
+            return String.Equals(fromAddress, other.fromAddress) &&
+                   String.Equals(toAddress, other.toAddress) &&
+                   String.Equals(subject, other.subject) &&
+                   String.Equals(body, other.body);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ReceivedMail) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (fromAddress != null ? fromAddress.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (toAddress != null ? toAddress.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (subject != null ? subject.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (body != null ? body.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        static ReceivedMail From(SmtpMessage smtpMessage)
+        {
+            var fromAddress = smtpMessage.FromAddress.Address;
+            var toAddress = smtpMessage.ToAddresses.Single().Address;
+            var subject = smtpMessage.Headers["Subject"];
+            var messagePart = smtpMessage.MessageParts.Single();
+            var body = messagePart.BodyData;
+            return new ReceivedMail(fromAddress, toAddress, subject, body);
+        }
+
+        public static ReceivedMail[] FromAll(SimpleSmtpServer smtpServer) =>
+            smtpServer.ReceivedEmail
+                .Select(From)
+                .OrderBy(x => x.toAddress)
+                .ToArray();
+    }
+}
