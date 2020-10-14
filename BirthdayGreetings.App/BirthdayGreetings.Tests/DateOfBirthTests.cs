f4d099ee7def811717946be870d88226e5d0ee55
Matteo Baglini <matteo.baglini@gmail.com>
2020-10-14 10:06:09 +0200

    add tests to DateOfBirth

---
+++ BirthdayGreetings.App/BirthdayGreetings.Tests/DateOfBirthTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using BirthdayGreetings.App;
+using Xunit;
+
+namespace BirthdayGreetings.Tests
+{
+    public class DateOfBirthTests
+    {
+        [Theory]
+        [InlineData(2020)]
+        [InlineData(2019)]
+        public void YesBirthday(int year)
+        {
+            var dateOfBirth = new DateOfBirth(11, 8);
+            var isBirthday = dateOfBirth.IsBirthday(new DateTime(year, 11, 8));
+            Assert.True(isBirthday);
+        }
+        
+        [Theory]
+        [InlineData(12, 8)]
+        [InlineData(11, 9)]
+        public void NoBirthday(int month, int day)
+        {
+            var dateOfBirth = new DateOfBirth(11, 8);
+            var isBirthday = dateOfBirth.IsBirthday(new DateTime(2020, month, day));
+            Assert.False(isBirthday);
+        }
+    }
+}
