f1fd9ea9bbfea55a160333b117c491702f011084
Matteo Baglini <matteo.baglini@gmail.com>
2020-10-28 11:29:03 +0100

    add more Smtp tests

--- BirthdayGreetings.App/BirthdayGreetings.App/SmtpGreetingsNotification.cs
+++ BirthdayGreetings.App/BirthdayGreetings.App/SmtpGreetingsNotification.cs
@@ -8,24 +8,52 @@ namespace BirthdayGreetings.App
     public class SmtpGreetingsNotification : IDisposable
     {
         readonly SmtpConfiguration configuration;
+        readonly ISendHooks hooks;
         readonly SmtpClient smtpClient;
 
         public SmtpGreetingsNotification(SmtpConfiguration configuration)
+            : this(configuration, new NoHooks())
+        {
+        }
+
+        public SmtpGreetingsNotification(SmtpConfiguration configuration, ISendHooks hooks)
         {
             this.configuration = configuration;
+            this.hooks = hooks;
             smtpClient = new SmtpClient(configuration.Host, configuration.Port);
         }
 
         public async Task SendBirthday(IList<EmailInfo> infos)
         {
             foreach (var info in infos)
+            {
+                hooks.OnBefore();
                 await smtpClient.SendMailAsync(configuration.Sender,
                     info.Email,
                     "Happy birthday!",
                     $"Happy birthday, dear {info.Name}!");
+                hooks.OnAfter();
+            }
         }
 
-        public void Dispose() => 
+        public void Dispose() =>
             smtpClient?.Dispose();
+    }
+
+    public class NoHooks : ISendHooks
+    {
+        public void OnBefore()
+        {
+        }
+
+        public void OnAfter()
+        {
+        }
+    }
+
+    public interface ISendHooks
+    {
+        void OnBefore();
+        void OnAfter();
     }
 }
---
+++ BirthdayGreetings.App/BirthdayGreetings.Tests/SmtpGreetingsNotificationTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Net.Mail;
+using System.Threading;
+using System.Threading.Tasks;
+using BirthdayGreetings.App;
+using BirthdayGreetings.Tests.Support;
+using FluentAssertions;
+using netDumbster.smtp;
+using Xunit;
+
+namespace BirthdayGreetings.Tests
+{
+    public class SmtpGreetingsNotificationTests
+    {
+        readonly SmtpConfiguration smtpConfiguration = new SmtpConfiguration
+        {
+            Sender = "foo@bar.com",
+            Host = "localhost",
+            Port = 5001
+        };
+
+        [Fact]
+        public async Task SendBirthday()
+        {
+            using var smtpServer = SimpleSmtpServer.Start(smtpConfiguration.Port);
+            using var notification = new SmtpGreetingsNotification(smtpConfiguration);
+
+            await notification.SendBirthday(new[]
+            {
+                new EmailInfo("foo", "a@a.com"),
+            });
+
+            ReceivedMail.FromAll(smtpServer)
+                .Should()
+                .BeEquivalentTo(new ReceivedMail(smtpConfiguration.Sender,
+                    "a@a.com",
+                    "Happy birthday!",
+                    "Happy birthday, dear foo!"));
+        }
+
+        [Fact]
+        public async Task ServerUnreachableOnFirstSend()
+        {
+            using var smtpServer = SimpleSmtpServer.Start(smtpConfiguration.Port);
+            using var notification = new SmtpGreetingsNotification(smtpConfiguration);
+
+            smtpServer.Stop();
+
+            var ex = await Assert.ThrowsAsync<SmtpException>(() =>
+                notification.SendBirthday(new[]
+                {
+                    new EmailInfo("foo", "a@a.com"),
+                }));
+            Assert.Equal(SmtpStatusCode.GeneralFailure, ex.StatusCode);
+        }
+
+        [Fact]
+        public async Task ServerUnreachableDuringSend()
+        {
+            using var smtpServer = SimpleSmtpServer.Start(smtpConfiguration.Port);
+            using var notification = new SmtpGreetingsNotification(smtpConfiguration,
+                new StopServerAfterOneSend(smtpServer));
+
+            var ex = await Assert.ThrowsAsync<SmtpException>(() => 
+                notification.SendBirthday(new[]
+                {
+                    new EmailInfo("foo", "a@a.com"),
+                    new EmailInfo("bar", "b@b.com"),
+                }));
+            Assert.Equal(SmtpStatusCode.GeneralFailure, ex.StatusCode);
+            
+            ReceivedMail.FromAll(smtpServer)
+                .Should()
+                .BeEquivalentTo(new ReceivedMail(smtpConfiguration.Sender,
+                    "a@a.com",
+                    "Happy birthday!",
+                    "Happy birthday, dear foo!"));
+        }
+
+        [Fact]
+        public void ServerUnreachable()
+        {
+            // NOTE: should not throws any SmtpException 
+            using var notification = new SmtpGreetingsNotification(smtpConfiguration);
+        }
+
+        class StopServerAfterOneSend : ISendHooks
+        {
+            readonly SimpleSmtpServer smtpServer;
+            bool isFirstSend;
+
+            public StopServerAfterOneSend(SimpleSmtpServer smtpServer)
+            {
+                this.smtpServer = smtpServer;
+                isFirstSend = true;
+            }
+
+            public void OnBefore()
+            {
+                if (!isFirstSend)
+                    smtpServer.Stop();
+            }
+
+            public void OnAfter()
+            {
+                if (isFirstSend)
+                    isFirstSend = false;
+            }
+        }
+    }
+}
